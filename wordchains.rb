e79e68a8dff559f7cfe8d60152c86a2eab49ca53
Tyler Hansen <tylernhansen@gmail.com>
2014-04-06 10:07:41 -0700

    added a dictionary file and an adjacent words finding helper method

--- wordchains.rb
+++ wordchains.rb
@@ -1,6 +1,23 @@
-class WordChainer
+require 'set'
 
-  def initialize(dict_file_name)
+class WordChainer
+  attr_accessor :dictionary
+
+  LETTERS = ('a'..'z').to_a.join('')
+
+
+  def initialize(dict_file_name = './dictionary.txt')
+    @dictionary = Set.new(File.new(dict_file_name).readlines.map(&:chomp))
+  end
+
+  def adjacent_words(word)
+    words = []
+    (0...word.length).each do |pos|
+      LETTERS.delete(word[pos]).chars.each do |new_char|
+        words << word.dup.tap{ |wrd| wrd[pos] = new_char }
+      end
+    end
+    words.select{ |wrd| self.dictionary.include?(wrd)}
   end
 
 
